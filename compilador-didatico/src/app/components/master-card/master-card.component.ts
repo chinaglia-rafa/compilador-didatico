65e4fea550d1b6a2304b4d29637ecba4dd5f380f
chinaglia <chinaglia.rafa@gmail.com>
2024-03-11 09:09:44 -0300

    feat(master-card): adiciona cards para cada parte da compilação

--- compilador-didatico/src/app/components/master-card/master-card.component.ts
+++ compilador-didatico/src/app/components/master-card/master-card.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { CUSTOM_ELEMENTS_SCHEMA, Component, Input } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import '@material/web/button/text-button';
 import { LiveIconComponent } from '../live-icon/live-icon.component';
 
@@ -27,11 +33,66 @@ export interface MasterCardItem {
   styleUrl: './master-card.component.scss',
 })
 export class MasterCardComponent {
+  /** Título do card */
   @Input('title') title: string = '';
+  /** Subtítulo do card */
   @Input('subhead') subhead: string = '';
+  /** Texto do botão de ação do card. Se estiver vazio, nenhum botão será exibido. */
   @Input('action') action: string = '';
+  /** Nome do ícone a ser exibido */
   @Input('icon') icon: string = '';
+  /** Lista de itens no formato MasterCardItem[] para serem exibidos na parte inferior do card */
   @Input('items') items: MasterCardItem[] = [];
+  /** Evento quando o card é clicado */
+  @Output('select') selectEmitter = new EventEmitter<MouseEvent>();
+  /** Evento quando o botão de ação é clicado */
+  @Output('actionClick') actionClickEmitter = new EventEmitter<MouseEvent>();
 
-  active = false;
+  /** Controla o estado de carregamento do componente */
+  protected loading = false;
+  /** Controla o estado de seleção do componente */
+  protected selected = false;
+
+  /**
+   * Função chamada ao clicar no componente
+   *
+   * @param event Evento de clique do Angular
+   */
+  clicked(event: MouseEvent): void {
+    console.log('click!');
+    this.selectEmitter.emit(event);
+  }
+
+  /**
+   * Função chamada ao clicar no botão de ação do componente
+   *
+   * @param event Evento de clique do Angular
+   */
+  actionClick(event: MouseEvent): void {
+    console.log('action click!');
+    event.stopPropagation();
+    this.actionClickEmitter.emit(event);
+  }
+
+  /**
+   * Alterna o estado do componente para "carregando...", modificando o ícone
+   */
+  toggleLoading(): void {
+    this.loading = !this.loading;
+  }
+
+  /**
+   * Alterna o estado do componente para "selecionado"
+   */
+  toggle(): void {
+    this.selected = !this.selected;
+  }
+
+  /**
+   * Atribui valor para selected
+   * @param state Valor a ser atribuído
+   */
+  setSelected(state: boolean): void {
+    this.selected = state;
+  }
 }
--- compilador-didatico/src/app/pages/home/home.component.ts
+++ compilador-didatico/src/app/pages/home/home.component.ts
@@ -1,19 +1,64 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import {
   MasterCardComponent,
   MasterCardItem,
 } from '../../components/master-card/master-card.component';
+import { NgScrollbarModule } from 'ngx-scrollbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MasterCardComponent],
+  imports: [MasterCardComponent, NgScrollbarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   itemsTest: MasterCardItem[] = [
     { label: 'linhas digitadas', value: '42', color: 'default' },
     { label: 'erros', value: '3', color: 'error' },
   ];
+
+  @ViewChild('codigoFonteCard')
+  codigoFonteCardComponent?: MasterCardComponent;
+  @ViewChild('analiseLexicaCard')
+  analiseLexicaCardComponent?: MasterCardComponent;
+  @ViewChild('simbolosCard')
+  simbolosCardComponent?: MasterCardComponent;
+  @ViewChild('analiseSintaticaCard')
+  analiseSintaticaCardComponent?: MasterCardComponent;
+  @ViewChild('analiseSemanticaCard')
+  analiseSemanticaCardComponent?: MasterCardComponent;
+  @ViewChild('geracaoDeCodigoCard')
+  geracaoDeCodigoCardComponent?: MasterCardComponent;
+  @ViewChild('otimizacaoDeCodigoCard')
+  otimizacaoDeCodigoCardComponent?: MasterCardComponent;
+  @ViewChild('resultadoFinalCard')
+  resultadoFinalCardComponent?: MasterCardComponent;
+
+  /** Lista de todos os cards presentes na tela inicial */
+  private cards: (MasterCardComponent | undefined)[] = [];
+
+  ngAfterViewInit(): void {
+    this.cards = [
+      this.codigoFonteCardComponent,
+      this.analiseLexicaCardComponent,
+      this.simbolosCardComponent,
+      this.analiseSintaticaCardComponent,
+      this.analiseSemanticaCardComponent,
+      this.geracaoDeCodigoCardComponent,
+      this.otimizacaoDeCodigoCardComponent,
+      this.resultadoFinalCardComponent,
+    ];
+  }
+
+  clicked(event: MouseEvent, el: MasterCardComponent): void {
+    el.toggleLoading();
+
+    this.cards.map((card) => card?.setSelected(false));
+
+    el.toggleLoading();
+
+    // setTimeout(() => el.toggleLoading(), 2000);
+    el.toggle();
+  }
 }
