216b6196bbc82ed6b2ef6e5a1df4eaaa9e2f1353
chinaglia <chinaglia.rafa@gmail.com>
2024-05-13 12:38:49 -0300

    WIP: gramática

--- compilador-didatico/src/app/pages/compiler/language-viewer/language-viewer.component.ts
+++ compilador-didatico/src/app/pages/compiler/language-viewer/language-viewer.component.ts
@@ -1,11 +1,16 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import {
   FirstList,
   FollowList,
   SyntacticAnalysisService,
 } from '../../../services/syntactic-analysis/syntactic-analysis.service';
 import { CommonModule } from '@angular/common';
-import { NgScrollbarModule } from 'ngx-scrollbar';
+import { NgScrollbar, NgScrollbarModule } from 'ngx-scrollbar';
 import '@material/web/textfield/outlined-text-field';
 import '@material/web/textfield/filled-text-field';
 import '@material/web/icon/icon';
@@ -28,14 +33,83 @@ export class LanguageViewerComponent {
 
   grammar: Grammar;
 
+  toBeHighlighted: string = '';
+
+  /** contagem de símbolos encontrados na busca */
+  totalFound: number = 0;
+  /** índice do símbolo focado no momento */
+  focused: number = -1;
+
+  foundCache: HTMLElement[] = [];
+
+  @ViewChild('content') contentElement: ElementRef;
+  @ViewChild('scrollbarComponent') scrollbar: NgScrollbar;
+  @ViewChild('search') searchComponent: ElementRef;
+
   constructor(private syntacticAnalysisService: SyntacticAnalysisService) {
     this.syntacticAnalysisService.ready.subscribe((status) => {
-      console.log('status', status);
       if (status === false) return;
       this.loading = false;
       this.grammar = this.syntacticAnalysisService.selectedGrammar;
       this.firsts = this.syntacticAnalysisService.firsts;
       this.follows = this.syntacticAnalysisService.follows;
     });
+  }
+
+  mouseEnter(name: string): void {
+    if (this.toBeHighlighted !== '') return;
+    this.toBeHighlighted = name;
+  }
+
+  mouseLeave(): void {
+    this.toBeHighlighted = '';
+  }
+
+  searchNext(event: KeyboardEvent): void {
+    if (event.key === 'Enter') {
+      if (this.totalFound === 0) return;
+      if (this.focused >= 0)
+        this.foundCache[this.focused].classList.remove('focused');
+      if (this.focused + 1 === this.totalFound) this.focused = 0;
+      else this.focused++;
+      this.foundCache[this.focused].classList.add('focused');
+      this.scrollbar.scrollToElement(this.foundCache[this.focused]);
+
+      return;
+    }
+
+    this.updateSearch();
+  }
+
+  updateSearch(): void {
+    this.focused = -1;
+
+    const found = this.contentElement.nativeElement.querySelectorAll('.symbol');
+    let total = 0;
+    this.foundCache = [];
+    for (let i = 0; i < found.length; i++) {
+      if (!found[i].classList.contains('found')) continue;
+      total++;
+      this.foundCache.push(found[i]);
+    }
+    this.totalFound = total;
+  }
+
+  selectSymbol(name: string): void {
+    this.resetSearch();
+    this.searchComponent.nativeElement.value = name;
+    this.searchComponent.nativeElement.focus();
+    setTimeout(() => this.updateSearch(), 200);
+  }
+
+  resetSearch(): void {
+    this.searchComponent.nativeElement.value = '';
+    this.totalFound = 0;
+    this.focused = -1;
+    this.contentElement.nativeElement
+      .querySelectorAll('.focused')
+      .forEach((el: HTMLElement) => {
+        el.classList.remove('focused');
+      });
   }
 }
